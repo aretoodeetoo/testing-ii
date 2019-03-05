e244dc1d13f05a539e4575181b2dd932a0d55ee1
croy <roy.christie95@gmail.com>
2019-03-04 22:22:02 -0600

    Added Dashboard and Display componenents, added material ui, basic boilerplating of React app done

---
+++ baseball-tests/src/components/Display.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Display extends React.Component{
+    render(){
+        return(
+            <>
+            <h1>Let's Play Ball!</h1>
+            <h2>Check Out The Score:</h2>
+            </>
+        );
+    }
+}
+
+export default Display;
