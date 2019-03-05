39253e1e0b78f5fe5d05a94e50a703173ae063a6
croy <roy.christie95@gmail.com>
2019-03-05 11:16:02 -0600

    Added increment functions to dashboard

--- baseball-tests/src/components/Dashboard.js
+++ baseball-tests/src/components/Dashboard.js
@@ -3,14 +3,49 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 
 class Dashboard extends React.Component{
+    state = {
+        strikeCount: 0,
+        ballCount: 0,
+        foulCount: 0,
+        hitCount: 0,
+    };
+
+    strikeIncrement = () => {
+        if (this.state.strikeCount === 3) {
+            this.setState({ strikeCount: 0, ballCount: 0})
+        } else {
+        this.setState({ strikeCount: this.state.strikeCount + 1 });
+        }
+    }
+
+    ballIncrement = () => {
+        if (this.state.ballCount === 4) {
+            this.setState({ strikeCount: 0, ballCount: 0})
+        } else {
+        this.setState({ ballCount: this.state.ballCount + 1 });
+        }
+    }
+
+    foulIncrement = () => {
+        this.setState({ foulCount: this.state.foulCount + 1 });
+    }
+
+    hitIncrement = () => {
+        if (this.state.hitIncrement === 1){
+            this.setState({ strikeCount: 0, ballCount: 0 });
+        } else {
+        this.setState({ hitCount: this.state.hitCount + 1 });
+        }
+    }
+
     render(){
         return(
             <>
             <h1>Welcome, Baseball Personnel!</h1>
-            <Button>Strike</Button>
-            <Button>Ball</Button>
-            <Button>Foul</Button>
-            <Button>Hit</Button>
+            <Button onClick={() => this.strikeIncrement()}>Strike</Button>
+            <Button onClick={() => this.ballIncrement()}>Ball</Button>
+            <Button onClick={() => this.foulIncrement()}>Foul</Button>
+            <Button onClick={() => this.hitIncrement()}>Hit</Button>
             </>
         );
     }
