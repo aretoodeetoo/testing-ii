2ffafd11dac22a559b03450cf8bb0da4d96f4972
croy <roy.christie95@gmail.com>
2019-03-05 11:32:17 -0600

    MVP functions are done, implementing tests

--- baseball-tests/src/tests/display.spec.js
+++ baseball-tests/src/tests/display.spec.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'react-testing-library';
 
-import Display from '../components/Display';
+import Dashboard from '../components/Dashboard';
 
-describe('<Display />', () => {
+describe('<Dashboard />', () => {
     it();
 });
