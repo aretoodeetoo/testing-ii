4f2804ab6513f03da8b02d71987dfe63d7b7e2b0
croy <roy.christie95@gmail.com>
2019-03-04 23:15:25 -0600

    Added test folder and display.spec.js file, added initial imports and a test skeleton

--- baseball-tests/src/components/Dashboard.js
+++ baseball-tests/src/components/Dashboard.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import Button from '@material-ui/core/Button';
+
 class Dashboard extends React.Component{
     render(){
         return(
             <>
             <h1>Welcome, Baseball Personnel!</h1>
+            <Button>Strike</Button>
+            <Button>Ball</Button>
+            <Button>Foul</Button>
+            <Button>Hit</Button>
             </>
         );
     }
